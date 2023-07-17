628ad3599fddb596cd41aff992a438af4bb94d14
FedorovMykhailo <fedorov.mykhailo@gmail.com>
2023-07-17 23:58:05 +0300

    update hw2

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,7 +2,7 @@ import css from "../Notification/Notification.module.css"
 
 const Notification = ({ message }) =>
 {
-   return <p>{message}</p>
+   return <p className={css.notification}>{message}</p>
 }
 
 export default Notification
